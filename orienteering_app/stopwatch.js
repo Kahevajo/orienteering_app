bc5621f659bade685bd03c1e9f359b0837064787
Matilda Carlson <a.matildacarlson@gmail.com>
2018-05-14 16:09:32 +0200

    timer initial

---
+++ orienteering_app/stopwatch.js
@@ -0,0 +1,42 @@
+let start = document.getElementById('start')
+let stop = document.getElementById('stop')
+let clear = document.getElementById('clear')
+let seconds = 0
+let minutes = 0
+let hours = 0
+let t;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+var timer = function () {
+    t = setTimeout(add, 1000);
+}
+timer();
+
+
+/* Start button */
+start.onclick = timer;
+
+/* Stop button */
+stop.onclick = function() {
+    clearTimeout(t);
+}
+
+/* Clear button */
+clear.onclick = function() {
+    textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
